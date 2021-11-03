5b6b6fe26e7521f797868763edea1c93d164ea8a
Jakub Wiśniewski <55205186+kubaczak@users.noreply.github.com>
2021-11-03 08:48:52 +0100

    Update SQL.sql

--- docs/SQL.sql
+++ docs/SQL.sql
@@ -1,5 +1,5 @@
 CREATE TABLE tag(id SERIAL PRIMARY KEY, name VARCHAR(255));
-CREATE TABLE tag_photo(id_tag INT, id_photo INT, PRIMARY KEY(id_tag, id_photo));
-CREATE TABLE photo(id SERIAL PRIMARY KEY, reference TEXT, likes INT, dislikes INT, views INT, title VARCHAR(255), description TEXT, location VARCHAR(255));
-CREATE TABLE comment(id SERIAL PRIMARY KEY, photo_id INT, user VARCHAR(255), content TEXT, date DATETIME);
+CREATE TABLE tag_photo(id_tag BIGINT UNSIGNED, id_photo BIGINT UNSIGNED, PRIMARY KEY(id_tag, id_photo));
+CREATE TABLE photo(id SERIAL PRIMARY KEY, album_id BIGINT UNSIGNED, reference TEXT, likes INT UNSIGNED, dislikes INT UNSIGNED, views INT UNSIGNED, title VARCHAR(255), description TEXT, location VARCHAR(255));
+CREATE TABLE comment(id SERIAL PRIMARY KEY, photo_id BIGINT UNSIGNED, user VARCHAR(255), content TEXT, date DATETIME);
 CREATE TABLE album(id SERIAL PRIMARY KEY, name VARCHAR(255));
